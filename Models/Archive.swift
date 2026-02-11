49a1a2202248ca0b71ad4b3912c4a1dbfcbc8c1a
c26jm <jmadover@gmail.com>
2026-02-11 14:35:01 -0500

    m

---
+++ Models/Archive.swift
@@ -0,0 +1,9 @@
+class Archive{
+    var Id: String
+    var CourseID: String
+    var storedQuestions: [String] //maybe not string, maybe question objects ask muizz
+
+    init(){
+        //empty for now
+    }
+}
