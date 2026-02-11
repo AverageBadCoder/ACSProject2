00e4bff75c434fa53972469a1921ada1097c537a
c26jm <jmadover@gmail.com>
2026-02-11 14:09:41 -0500

    m

---
+++ Models/ReputationBadge.swift
@@ -0,0 +1,11 @@
+class repBadges{
+    var badgeLevel: Int
+    var badgeName: String
+    var badgeDescription: String
+    var badgeIcon: String
+    var badgeType: String //maybe not string
+
+    init(){
+        //empty for now
+    }
+}
